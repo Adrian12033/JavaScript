1099a1f449a6400de44f5911d62fbe09646e131c
Adrian Ordoñez Rossello <146111056+Adrian12033@users.noreply.github.com>
2024-04-26 16:43:15 +0000

    24 listo

--- IntroduccionJS/js/24.js
+++ IntroduccionJS/js/24.js
@@ -14,14 +14,14 @@
 //     }
 // }
 
-// const carrito = [
-//     {nombre: "Monitor 20 pulgadas", precio: 500},
-//     {nombre: " Tele", precio: 300},
-//     {nombre: "Ipad", precio: 200},
-//     {nombre: "Movil", precio: 700},
-//     {nombre: "Teclado", precio: 300},
-//     {nombre: "PC", precio: 1500},
-// ]  
+const carrito = [
+  { nombre: "Monitor 20 pulgadas", precio: 500 },
+  { nombre: "Tele", precio: 300 },
+  { nombre: "Ipad", precio: 200 },
+  { nombre: "Movil", precio: 700 },
+  { nombre: "Teclado", precio: 300 },
+  { nombre: "PC", precio: 1500 },
+];
 
 // for (i = 0; i< carrito.length; i++) { // el .length es para que recorra todo lo que hay dentro del carrito
 //     console.log(carrito[i].nombre);
@@ -30,18 +30,25 @@
 
 //WHILE LOOP //se ejecuta mientras una condicion sea verdadera
 
-let i = 0; //indice
+// let i = 20; //indice
 
-while (i < 100) { // condicion
+// while (i < 10) { // condicion
+    
+//     console.log('No aparece nada') //no aparece nada ya que el while loop solo funciona cuando la condición se cumple en este caso le eestoy diciendo que empiece por 10 pero el indice empieza por 20.
 
-    if (i % 2 === 0) {
-        console.log(`El numero ${i} es PAR`);
-    } else{
-        console.log(`El número ${i} es IMPAR`);
-    }
-
-    i++; //incremento
-}
+//   i++; //incremento
+// }
 
 
-// DOO WHILE LOOP
+// DOO WHILE LOOP // la principal diferencia entre el while loop y el do while loop es que el while loop evalua la condición y si es falso no actua en cambio el do while loop no evalua y lo hace aunque sea falso
+
+// DIFERENCIAAAAAA ENTRE WHILE Y DO WHILE: el do while se ejecuta al menos una vez aunque la condfición no se cumpla y el while tiene que revisar que la condición se cumpla para que se pueda utilizar
+
+let i = 200; 
+
+do { //el do while ejecuta el codigo al menos una vez y después evalua
+ 
+    console.log(i);
+
+    i++;
+} while( i < 10);
