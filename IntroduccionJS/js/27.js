277661110c47ca6dc980e181db26c9a29c3eaddc
Adrian Ordoñez Rossello <146111056+Adrian12033@users.noreply.github.com>
2024-04-27 07:44:39 +0000

    listo 27

--- IntroduccionJS/js/27.js
+++ IntroduccionJS/js/27.js
@@ -1,12 +1,22 @@
 //PROGRAMACION ORIENTADA A OBJETOS
 
-
 /*OBJECT LITERAL*/
+
 const producto = {
-    nombre: 'Monitor',
-    precio: 400
+  nombre: "Monitor",
+  precio: 400,
+};
+
+//DIFERENCIA ENTRE CLASE Y OBJETO: un objeto tiene diferentes variables en una sola, las clases
+
+//OBJECT CONSTRUCTOR //es mas dinamico
+
+function Producto(nombre, precio) {   //Producto es una clase
+  this.nombre = nombre;
+  this.precio=precio;
 }
 
-//OBJECT CONSTRUCTOR
+const producto2 = new Producto("Monitor curvo de 49 pulgadas", 800);
+const producto3 = new Producto("Laptop", 300);
 
-
+console.log(producto2);
