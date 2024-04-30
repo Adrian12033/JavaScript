2f4a930567363b64072ee7abf90baff7d0bb4dee
Adrian12033 <adrianordonez1203@gmail.com>
2024-04-30 15:45:32 +0200

    27 listo

--- IntroduccionJS/js/27.js
+++ IntroduccionJS/js/27.js
@@ -11,12 +11,37 @@ const producto = {
 
 //OBJECT CONSTRUCTOR //es mas dinamico
 
-function Producto(nombre, precio) {   //Producto es una clase
+function Cliente(nombre, apellido) {
   this.nombre = nombre;
-  this.precio=precio;
+  this.apellido = apellido;
 }
+
+Cliente.prototype.formatearCliente = function () {
+  return `El cliente ${this.nombre} tiene el apellido: ${this.apellido}`;
+};
+
+const cliente = new Cliente("Adrian", "Ordoñez Rossello");
+
+
+function Producto(nombre, precio) {
+  //Producto es una clase
+  this.nombre = nombre;
+  this.precio = precio;
+}
+
+//El prototype solo se podrá usar en un obejto en específico
+
+Producto.prototype.formatearProducto = function () {
+  return `El producto ${this.nombre} tiene un precio de: ${this.precio}€`;
+};
 
 const producto2 = new Producto("Monitor curvo de 49 pulgadas", 800);
 const producto3 = new Producto("Laptop", 300);
 
-console.log(producto2);
+function formatearProducto() {
+  return `El producto ${producto.nombre} tiene un precio de: ${producto.precio}€`;
+}
+
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
+console.log(cliente.formatearCliente());
