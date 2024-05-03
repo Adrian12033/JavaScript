afc1ed6470e4b7b310d472ac3a8a4eacd31575f2
Adrian12033 <adrianordonez1203@gmail.com>
2024-05-03 20:11:51 +0200

    28

--- IntroduccionJS/js/28.js
+++ IntroduccionJS/js/28.js
@@ -21,3 +21,23 @@ const producto3 = new Producto("Laptop", 300);
 console.log(producto2);
 console.log(producto3);
 console.log(producto2.devolverPrecio());
+
+//HERENCIA
+
+class Libro extends Producto { 
+
+    constructor(nombre, precio, isbn){
+        super(nombre, precio); // esto lo que está haciendo es coger de constructor de producto
+        this.isbn = isbn; // ya que en el constructor de producto no está isbn lo añadimos en el contructor de Libro 
+    }
+
+    formatearLibro(){
+        return `El libro ${this.nombre} con un precio de ${this.precio}€ y tiene el siguiente ISBN ${this.isbn}`
+    }
+}
+
+const libro = new Libro ("La guerra de las galaxias", 12.95, "75483745837654");
+
+console.log(libro.formatearLibro());
+
+
