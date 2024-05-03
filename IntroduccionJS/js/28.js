0bfd9c76d6400c60a6f37460efc79627fb258999
Adrian12033 <adrianordonez1203@gmail.com>
2024-05-03 19:40:27 +0200

    28

--- IntroduccionJS/js/28.js
+++ IntroduccionJS/js/28.js
@@ -1,7 +1,23 @@
 // CLASES
 
 class Producto {
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
+    formatearProducto(){
+        return `El producto ${this.nombre} tiene un precio de: ${this.precio}€`;
+    }
+
+    devolverPrecio() {
+        return `El precio del ${this.nombre} es de ${this.precio}`;
+    }
 }
 
-const producto = new Producto();
+const producto2 = new Producto("Monitor curvo de 49 pulgadas", 800);
+const producto3 = new Producto("Laptop", 300);
+
+console.log(producto2);
+console.log(producto3);
+console.log(producto2.devolverPrecio());
