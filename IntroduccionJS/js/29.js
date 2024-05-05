76a0a87ecd0bfeb4b7c8e4bf9eb307d74ddcc104
Adrian12033 <adrianordonez1203@gmail.com>
2024-05-05 20:54:52 +0200

    try catch

---
+++ IntroduccionJS/js/29.js
@@ -0,0 +1,17 @@
+//TRY CATCH 
+
+const numero1 = 20;
+const numero2 = 30;
+
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error);
+}
+
+console.log(numero1);
+console.log(numero3);
+console.log(numero2);
+
+//asi como he realizado el console log ejecuta el primero pero a la hora de ejecutar el segundo como no lo encuentra ya me da un error y no me ejecutas el numero 2 el cual si que existe, 
+//para esto usamos el try, para que coja el numeor dos. 
